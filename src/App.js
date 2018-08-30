b889f8c6c770bbb40f8c9807f359d8ff6eb5b9af
chris.lego <poputeacristi@gmail.com>
2018-08-30 20:21:26 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
-import cd from './assets/cd-animated-gif-11.gif';
+import cd from './assets/cs_image.png';
+import piedpiper_gray from './assets/piedpier-gray.png';
+import piedpiper_red from './assets/piedpiper-red.png';
 import piedpiper_violet from './assets/piedpiper-violet.svg';
-import piedpiper_green from './assets/piedpiper-green.svg';
+import piedpiper_green from './assets/piedpiper-green.png';
 import './App.css';
+
+const renderPiper = () => {
+  let result = [];
+  let leftSize = 80;
+  for (let i = 0; i <= 100; i++) {
+    if (i % 2) {
+      result.push(
+        <img src={piedpiper_red} className="footer-image-piper-red" style={{ left: leftSize * i }} />,
+        <img src={piedpiper_green} className="footer-image-piper-green footer-image-piper" style={{ left: leftSize * i }} />
+      );
+    } else {
+      result.push(
+        <img src={piedpiper_red} className="footer-image-piper-red footer-image-piper" style={{ left: leftSize * i }} />,
+        <img src={piedpiper_green} className="footer-image-piper-green footer-image-piper-invert" style={{ left: leftSize * i }} />
+      );
+    }
+  }
+  return result;
+};
 
 class App extends Component {
   render() {
@@ -10,13 +31,13 @@ class App extends Component {
       <div className="App">
         <header className="header">
           <img src={cd} className="cd-img" alt="logo" />
-          <h1 className="banner-title">COMING SOONE</h1>
+          <span className="banner-title"><h1>COMING SOONE</h1></span>
           <img src={cd} className="cd-img" alt="logo" />
         </header>
         <main>
           <section className="sidebar">
             <div className="sidebar-side-logo animated-chinese-sidebar-text">
-              <img src={piedpiper_violet} className="sidebar-side-logo-image"/>
+              <img src={piedpiper_violet} className="sidebar-side-logo-image" />
             </div>
             <div className="sidebar-side-logo-text">
               <div className="logo-text pied-logo-text">pied</div>
@@ -25,19 +46,28 @@ class App extends Component {
             <div className="sidebar-side-logo-text-shiny animated-chinese-sidebar-text">敬臟颶待</div>
           </section>
           <section className="main-content">
-            <div className="main-content-children">
-            </div>
-            <div className="live-count-container">
-              The page has been visited <span className="count">0002112</span> times!
+            <div>
+              <div className="main-content-children">
+                <div className="main-content-title">
+                  <div className="main-content-title-pied">pied</div>
+                  <div className="main-content-title-piper">piper</div>
+                </div>
+                <img src={piedpiper_green} className="big-piper" />
+                <img src={piedpiper_gray} className="big-piper big-piper-animation" />
+              </div>
+              <div className="live-count-container">
+                The page has been visited <span className="count">0001934</span> times!
+              </div>
             </div>
           </section>
           <section className="sidebar">
             <div className="sidebar-side-logo animated-chinese-sidebar-text-invert">
-              <img src={piedpiper_green} className="sidebar-side-logo-image"/>
+              <img src={piedpiper_green} className="sidebar-side-logo-image" />
             </div>
             <div className="sidebar-side-logo-text">
               <div className="logo-text pied-logo-text">pied</div>
-              <div className="logo-text piper-logo-text">piper</div></div>
+              <div className="logo-text piper-logo-text">piper</div>
+            </div>
             <div className="sidebar-side-logo-text-shiny animated-chinese-sidebar-text-invert">敬臟颶待</div>
           </section>
         </main>
@@ -56,6 +86,7 @@ class App extends Component {
             <div className="chinese-text-children animated-chinese-footer-text">互臟廳眈忨囀忨</div>
           </div>
           <div className="pied-pier-footer-img">
+            {renderPiper()}
           </div>
           <div className="copyright-container">
             <div>
@@ -66,7 +97,7 @@ class App extends Component {
               <span className="footer-pied-piper-name">PiedPieperChina</span>
               <span>homepage</span>
             </div>
-            <span>terms of use</span>
+            <span className="footer-terms">terms of use</span>
           </div>
         </footer>
       </div>
