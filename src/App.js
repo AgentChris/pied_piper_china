0aca146c65ed4315d57cb4e4385aeada7f44f65f
chris.lego <poputeacristi@gmail.com>
2018-09-01 09:23:54 +0300

    added easter eggs

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import song from './assets/Abba - Super Trouper.mp3';
 import piedpiper_red from './assets/piedpiper-red.png';
 import piedpiper_violet from './assets/piedpiper-violet.svg';
 import piedpiper_green from './assets/piedpiper-green.png';
+import cool from './assets/bad ass.gif';
+import showIsGoneSrc from './assets/isgone.gif';
+import showIsOcasionSrc from './assets/ocasion.gif';
+import handshakeSrc from './assets/rad.gif';
+import antoanSrc from './assets/5b8a2866de7c0325233531.gif';
+import ericDeatchSrc from './assets/erichdeath.gif';
 import './App.css';
 
 const renderPiper = () => {
@@ -31,25 +37,84 @@ const renderPiper = () => {
 };
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showCool: false,
+      showIsGone: false,
+      showPipa: false,
+      atLeastOneshowIsGone: false,
+      atLeastOneshowCool: false,
+      atLeastOneshowsOcasion: false,
+      message: false,
+      optionAnswer: false,
+      ericDeatch: false,
+      click4times: 0,
+      answer: "",
+      showIsOcasion: false,
+      showCongrats: false,
+    };
+  }
+
   componentDidMount() {
     ReactGA.initialize('UA-125025103-1');
     ReactGA.pageview(window.location.pathname + window.location.search);
   }
 
+  showBadASs = () => {
+    this.setState((state) => ({ showCool: !state.showCool, atLeastOneshowCool: true }));
+  };
+
+  changeAnswer = (e) => {
+    this.setState({ answer: e.target.value });
+  };
+
+  showPipa = () => {
+    this.setState((state) => ({ click4times: state.click4times + 1 }));
+    if (this.state.click4times > 3) {
+      this.setState({ optionAnswer: true, ericDeatch: true });
+    }
+  };
+
+  checkAnswer = () => {
+    const { answer } = this.state;
+    if (answer === "the best pied pipa") {
+      this.setState({ showCongrats: true, });
+    } else {
+      this.setState({ message: true });
+    }
+  };
+
+  showIsGone = () => {
+    this.setState((state) => ({ showIsGone: !state.showIsGone, atLeastOneshowIsGone: true }));
+  };
+  showIsOcasion = (val) => {
+    this.setState((state) => ({ showIsOcasion: val, atLeastOneshowsOcasion: true }));
+  };
+
   render() {
+    const {
+      showCool, showIsGone, showIsOcasion, answer, showCongrats, atLeastOneshowIsGone,
+      atLeastOneshowCool, atLeastOneshowsOcasion, message, optionAnswer, ericDeatch
+    } = this.state;
     return (
       <div className="App">
         <header className="header">
           <img src={cd} className="cd-img" alt="logo" />
           <span className="banner-title"><h1>COMING SOONE</h1></span>
-          <img src={cd} className="cd-img" alt="logo" />
+          <img src={cd} className="cd-img" alt="logo" onMouseEnter={() => {
+            this.showIsOcasion(true);
+          }} />
+          {showIsOcasion ? <span className="z-index-top"><img src={showIsOcasionSrc} onMouseLeave={() => {
+            this.showIsOcasion(false);
+          }} />"pied"</span> : false}
         </header>
         <main>
           <section className="sidebar">
             <div className="sidebar-side-logo animated-chinese-sidebar-text">
               <img src={piedpiper_violet} className="sidebar-side-logo-image" />
             </div>
-            <div className="sidebar-side-logo-text">
+            <div className="sidebar-side-logo-text" onClick={this.showPipa}>
               <div className="logo-text pied-logo-text">pied</div>
               <div className="logo-text piper-logo-text">piper</div>
             </div>
@@ -58,12 +123,13 @@ class App extends Component {
           <section className="main-content">
             <div>
               <div className="main-content-children">
+                {showCool ? <span><img src={cool} />"the"</span> : false}
                 <div className="main-content-title">
                   <div className="main-content-title-pied">pied</div>
                   <div className="main-content-title-piper">piper</div>
                 </div>
-                <img src={piedpiper_green} className="big-piper" />
-                <img src={piedpiper_gray} className="big-piper big-piper-animation" />
+                <img src={piedpiper_green} className="big-piper" onClick={this.showBadASs} />
+                <img src={piedpiper_gray} onClick={this.showBadASs} className="big-piper big-piper-animation" />
               </div>
               <div className="live-count-container">
                 The page has been visited <span className="count">0001934</span> times!
@@ -77,6 +143,7 @@ class App extends Component {
             <div className="sidebar-side-logo-text">
               <div className="logo-text pied-logo-text">pied</div>
               <div className="logo-text piper-logo-text">piper</div>
+              {ericDeatch ? <span className="z-index-top"><img src={ericDeatchSrc} />"pipa"</span> : null}
             </div>
             <div className="sidebar-side-logo-text-shiny animated-chinese-sidebar-text-invert">敬臟颶待</div>
           </section>
@@ -92,13 +159,26 @@ class App extends Component {
           <div className="chinese-text-container">
             <div className="chinese-text-children animated-chinese-footer-text-green">互臟廳眈忨囀忨</div>
             <div className="chinese-text-children animated-chinese-footer-text">互臟廳眈忨囀忨</div>
-            <div className="chinese-text-children animated-chinese-footer-text-green">互臟廳眈忨囀忨</div>
+            <div className="chinese-text-children animated-chinese-footer-text-green">互臟廳眈忨<span
+              onClick={this.showIsGone}>囀</span>忨
+            </div>
             <div className="chinese-text-children animated-chinese-footer-text">互臟廳眈忨囀忨</div>
           </div>
+          {showIsGone ? <span><img src={showIsGoneSrc} />"best"</span> : false}
           <div className="pied-pier-footer-img">
             {renderPiper()}
           </div>
           <div className="custom-code">
+            {optionAnswer && atLeastOneshowCool && atLeastOneshowsOcasion && atLeastOneshowIsGone ?
+              <div>
+                <label>Enter answer to joine pied pipa china</label>
+                <input type="text" value={answer} onChange={this.changeAnswer} placeholder="check answer" />
+                {message ? <span><img src={antoanSrc} />"nope try again"</span> : null}
+                <button onClick={this.checkAnswer}>checku</button>
+                {showCongrats ? <img src={handshakeSrc} /> : null}
+              </div>
+              : null
+            }
             <div>
               <div>We are a community of developers who want to build all silicon valley TV series apps. To continue to
                 develop
